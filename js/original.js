c1361ba86adacf47baa37f53fb01451f05a104d9
mismith0227 <doraepon2216@gmail.com>
2016-04-13 07:28:55 +0900

    first commit

---
+++ js/original.js
@@ -0,0 +1,10 @@
+( function( $ ) {
+  $(function(){
+      $('.page_item_has_children').on('click', function(e){
+        e.preventDefault();
+        console.log('test');
+        $(this).find('.children').css({'display': 'block'});
+      });
+  });
+
+} )( jQuery );

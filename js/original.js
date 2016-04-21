31bc2ffe8438ed820fce2f94c8a48192bba5c663
mismith0227 <doraepon2216@gmail.com>
2016-04-21 21:25:39 +0900

    ヘッダーの修正

--- js/original.js
+++ js/original.js
@@ -1,56 +1,33 @@
 ( function( $ ) {
   $(function(){
 
+
     var setting = {
       tablet: 1024
     }
 
-    var head_dist = $('#site-navigation').offset().top;
-    var nav_height = $('#site-navigation').outerHeight() + 2;
-    var hdtar = head_dist + nav_height + 50;
-
-    // headroom
-    var myElement = document.querySelector(".main-navigation");
-    var headroom  = new Headroom(myElement, {
-      offset : hdtar
-    });
-    headroom.init();
-
     // headerstyle
     function headstyle() {
       var wW = $(window).width();
+      var myElement = document.querySelector(".main-navigation");
+      var headroom  = new Headroom(myElement, {
+        offset : 300
+      });
+
       if(wW < setting.tablet) {
-        $('.headroom').css({'top': 'inherit'});
+        headroom.destroy();
+        $('.main-navigation').css({'top': 100 + '%'});
       } else {
-        $('.headroom').css({'top': -nav_height});
+        headroom.init();
+        $('.main-navigation').css({'top': 0});
       }
 
     	if ( $('#wpadminbar').length ) {
-    		console.log('test');
+
     	}
 
-      var $nav = $('.primary-menu');
-      var $navli = $nav.children('li');
-
-      var sumli = 0;
-      var linum = $navli.length;
-      for(i = 0; i < linum; i++) {
-        sumli += $navli.eq(i).outerWidth(true);
-      }
-
-      if(wW < setting.tablet) {
-        $nav.css({
-          'width': sumli + 10
-        });
-      } else {
-        $nav.css({
-          'width': 'auto'
-        });
-      }
-
-
-      console.log(sumli);
     }
+
     headstyle();
     $(window).on('resize', function(){
       headstyle();
@@ -58,11 +35,10 @@
 
     $(window).on('scroll', function(){
       var scrval = $(window).scrollTop();
-      if(scrval > hdtar) {
-        $('.headroom').addClass('headfixed');
+      var nav_height = $('#site-navigation').outerHeight();
+      if(scrval > 300) {
         $('#content').css({'margin-top': nav_height});
       } else {
-        $('.headroom').removeClass('headfixed');
         $('#content').css({'margin-top': 0});
       }
     });

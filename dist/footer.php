6ec012938ffdeb58174c69ab3e35a2d855b8245c
mismith0227 <doraepon2216@gmail.com>
2016-04-27 08:21:37 +0900

    申請準備

--- dist/footer.php
+++ dist/footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'modernize' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'modernize' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'modernize' ), 'modernize', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'modernize' ), 'modernize', '<a href="https://profiles.wordpress.org/mismith227" rel="designer">mismith227</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->

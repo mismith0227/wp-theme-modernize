8b35ae12957aba3c98591b25367f2ec3b91ba959
mismith0227 <doraepon2216@gmail.com>
2016-04-26 20:15:50 +0900

    記事ページの調整

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscorestheme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'underscorestheme' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscorestheme' ), 'underscorestheme', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscorestheme' ), 'underscorestheme', '<a href="https://profiles.wordpress.org/mismith227" rel="designer">mismith227</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->

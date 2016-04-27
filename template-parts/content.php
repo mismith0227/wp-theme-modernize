f8e48af54cd0698a087fcfedbd4d1e3c494caf3b
mismith0227 <doraepon2216@gmail.com>
2016-04-27 20:59:09 +0900

    固定ページのデザイン変更 fixes #6

--- template-parts/content.php
+++ template-parts/content.php
@@ -32,18 +32,19 @@
 				}
 			}
 		?>
+		<div class="entry-info">
+			<?php
+				if ( is_single() ) {
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				} else {
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				}
 
-		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
-
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php modernize_posted_on(); ?>
-		</div><!-- .entry-meta -->
+			if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php modernize_posted_on(); ?>
+			</div><!-- .entry-meta -->
+		</div>
 
 		<?php
 		endif; ?>

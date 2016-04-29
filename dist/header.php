fd341f11b97781fe67e6c7204098621114217f8d
mismith0227 <doraepon2216@gmail.com>
2016-04-29 19:55:43 +0900

    edit readme

---
+++ dist/header.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package modernize
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'modernize' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-header-inner">
+			<div class="site-branding">
+				<?php
+				if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+				endif;
+
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php
+				endif; ?>
+			</div><!-- .site-branding -->
+
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<?php if ( has_nav_menu( 'primary' ) ) : ?>
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'primary',
+								'menu_class'     => 'primary-menu',
+							 ) );
+						?>
+				<?php endif; ?>
+				<button class="toggle-btn js-toggle">
+					<span></span>
+				</button>
+			</nav><!-- #site-navigation -->
+		</div><!-- .site-header-inner -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
---
+++ dist/template-parts/content-page.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package modernize
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+			if ( has_post_thumbnail() ) {
+				if ( is_single() ) {
+		?>
+		<div class="wrap-thumb">
+			<?php the_post_thumbnail(); ?>
+		</div>
+		<?php
+			} else {
+		?>
+
+			<div class="wrap-thumb">
+				<a href="<?php echo get_permalink(); ?>">
+				<?php the_post_thumbnail(); ?>
+				</a>
+			</div>
+		<?php
+				}
+			}
+		?>
+		<div class="entry-info">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</div>
+
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			the_content();
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'modernize' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php
+			edit_post_link(
+				sprintf(
+					/* translators: %s: Name of current post */
+					esc_html__( 'Edit %s', 'modernize' ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+		?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->

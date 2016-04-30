782a15489ed817d066b7e7833cb2eb2fd8d28fde
mismith0227 <doraepon2216@gmail.com>
2016-04-30 11:16:25 +0900

    ギャラリーにzoom.jsがあたらないように fixed #26

--- js/main.js
+++ js/main.js
@@ -39,6 +39,7 @@
 
     // zoom.js
     $('.single-post .entry-content img').attr('data-action', 'zoom');
+    $('.single-post .entry-content div[class*="gallery"] img').attr('data-action', '');
 
     var Drawer = (function() {
 

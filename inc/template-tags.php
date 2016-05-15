f8fc1974d1e001349711f970da0832c91e7628c9
mismith0227 <doraepon2216@gmail.com>
2016-05-15 11:59:55 +0900

    スタイル調整と更新日の表示を削除 fixed #35

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -13,9 +13,6 @@ if ( ! function_exists( 'modernize_posted_on' ) ) :
  */
 function modernize_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
 
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),

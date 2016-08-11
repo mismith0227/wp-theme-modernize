5a82746a90af78369590aeb9d9ef1864ddb983bb
mismith0227 <doraepon2216@gmail.com>
2016-08-11 17:51:00 +0900

    Travis テスト 4

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -32,4 +32,4 @@ git config user.name "Travis CI"
 git config user.email "travis@example.com"
 git add .
 git commit --quiet -m "Deploy from travis"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" travis_test:release > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" travis_test:release

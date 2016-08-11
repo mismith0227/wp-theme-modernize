787d7b6a69e2e1c260930e77c6eb3f0f6de0b931
mismith0227 <doraepon2216@gmail.com>
2016-08-11 17:41:21 +0900

    Travis テスト 3

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,8 +7,8 @@ if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
 	exit
 fi
 
-if [[ "master" != "$TRAVIS_BRANCH" ]]; then
-	echo "Not on the 'master' branch."
+if [[ "travis_test" != "$TRAVIS_BRANCH" ]]; then
+	echo "Not on the 'travis_test' branch."
 	exit
 fi
 
@@ -32,4 +32,4 @@ git config user.name "Travis CI"
 git config user.email "travis@example.com"
 git add .
 git commit --quiet -m "Deploy from travis"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:release > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" travis_test:release > /dev/null 2>&1

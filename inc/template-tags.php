64c910e3f5ffc02748bf4b18976f6dda0ee850ee
mismith0227 <doraepon2216@gmail.com>
2016-09-13 08:19:39 +0900

    add edit date
    fixed #86

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -14,6 +14,13 @@ if ( ! function_exists( 'modernize_posted_on' ) ) :
 function modernize_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) && !is_home() && !is_archive() && !is_search() ) {
+		$time_string = '
+		<span class="publised--label">Posted on<time class="entry-date published" datetime="%1$s">%2$s</time></span>
+		<span class="update--label">Edit on<time class="updated" datetime="%3$s">%4$s</time></span>
+		';
+	}
+
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() ),
@@ -21,17 +28,14 @@ function modernize_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'modernize' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
+	$posted_on = sprintf( $time_string );
 
 	$byline = sprintf(
 		esc_html_x( 'by %s', 'post author', 'modernize' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="posted--on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 
 }
 endif;
